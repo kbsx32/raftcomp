eebef573ae987621e5d20fab7f808ba75130879e
kbsx32 <kbsx32@yandex.ru>
2015-09-20 23:32:46 +0000

    writing lap info. in progress.

---
+++ src/gui/lap_table_team.cpp
@@ -0,0 +1,7 @@
+#include "lap_table_team.h"
+
+lap_table_team::lap_table_team()
+{
+
+}
+
---
+++ src/gui/lap_table_team.h
@@ -0,0 +1,11 @@
+#ifndef LAP_TABLE_TEAM_H
+#define LAP_TABLE_TEAM_H
+
+
+class lap_table_team
+{
+public:
+	lap_table_team();
+};
+
+#endif // LAP_TABLE_TEAM_H
